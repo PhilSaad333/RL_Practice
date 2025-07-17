9ff9783e7efbe4434d77ac708b8d0f8467c82ddc
Phil Saad <phil.saad333@gmail.com>
2025-07-17 02:06:45 -0700

    Update collect_rollouts.py

--- rl_training/runners/collect_rollouts.py
+++ rl_training/runners/collect_rollouts.py
@@ -108,7 +108,7 @@ class RolloutCollector:
             question   = self.prompt_sampler.id2text[prompt_id]
             prompt_text = question + "\n<think>\n"
 
-            batch = tokenizer(prompts, return_tensors="pt", padding=True)  # already left-pad
+            batch = self.tokenizer(prompts, return_tensors="pt", padding=True)  # already left-pad
             prompt_ids     = batch["input_ids"].to(device)
             attention_mask = batch["attention_mask"].to(device)
 
