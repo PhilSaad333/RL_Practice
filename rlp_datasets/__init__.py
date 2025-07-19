8e19c45cf91d03638a85742148ab22a711e4bd0e
Phil Saad <phil.saad333@gmail.com>
2025-07-18 17:36:38 -0700

    Create __init__.py

---
+++ rlp_datasets/__init__.py
@@ -0,0 +1,7 @@
+# rlp_datasets/__init__.py
+
+from .registry import DATASET_REGISTRY
+
+# (Optionally also import any builders here so users can do
+#   import rlp_datasets; rlp_datasets.DATASET_REGISTRY
+# )
